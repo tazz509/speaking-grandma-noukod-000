dfa69d1555194497d1f30a97af3094cb05542e35
carly <carly@noukod.com>
2018-09-18 15:41:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma phrase
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase
-    "huh?! speak up, sonny!"
+    "HUH?! SPEAK UP, SONNY!"
   else
     "NO, NOT SINCE 1938!"
   end
